4140d64775085606d5ef8ac20c34b7337bc74a49
Gio <143645611+GioDelG@users.noreply.github.com>
2023-09-09 00:36:33 -0300

    fix: add coment to files for explanation

--- src/server.js
+++ src/server.js
@@ -1,24 +1,24 @@
+// Modo 'strict' para prevenir ciertos errores comunes en JavaScript
 'use strict';
+// Importa la librería Express para construir el servidor
 const express = require('express');
-
-// Variables de entorno
+// Obtiene las variables de entorno, con valores predeterminados si no están definidas
 const PORT = process.env.PORT || 8080;
 const HOST = process.env.HOST || '0.0.0.0';
 const greetingMessage = process.env.GREETING_MESSAGE || 'Hello World';
-
-// App
+// Crea una instancia de la aplicación Express
 const app = express();
-
+// Define una ruta raíz que responderá con el mensaje de saludo
 app.get('/', (req, res) => {
     res.send(greetingMessage);
 });
-
+// Define una ruta de salud que se puede utilizar para comprobar el estado del servidor
 app.get('/health', (req, res) => {
     res.status(200).send('Healthy');
 });
-
+// Inicia el servidor Express en el puerto y host especificados
 app.listen(PORT, HOST, () => {
     console.log(`Running on http://${HOST}:${PORT}`);
 });
-
+// Exporta la aplicación para usarla en otros módulos si es necesario
 module.exports = app;
